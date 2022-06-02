0fe6f76f5ce7a0d9915687e2127f6956688409f8
Prajna Hegde <prajnahegdeg@gmail.com>
2022-06-02 19:17:21 +0530

    Added StringCalculator File

---
+++ StringCalculator/StringCalculator.swift
@@ -0,0 +1,16 @@
+//
+//  StringCalculator.swift
+//  StringCalculator
+//
+//  Created by Hegde, Prajna on 02/06/22.
+//
+
+import Foundation
+
+class StringCalculator {
+    
+    func add(numberString: String) -> Int {
+        
+        return 0
+    }
+}
