b948316ac2f7bb9feae0068a729c853a2ed2696d
Prajna Hegde <prajnahegdeg@gmail.com>
2022-06-03 15:22:57 +0530

    implemented the scenario for handling any length delimiter

--- StringCalculator/StringCalculator.swift
+++ StringCalculator/StringCalculator.swift
@@ -10,7 +10,7 @@ import Foundation
 class StringCalculator {
     
     var calledCount = 0
-    
+    var newLineDelimiter = "\n"
     func add(numberString: String) throws -> Int {
         calledCount += 1
         if(numberString.isEmpty) {
@@ -36,9 +36,9 @@ class StringCalculator {
     }
     
     private func addString(numberString: String) throws -> Int {
+        
         let result = getDelimiter(inputString: numberString)
-        let characterSet = result.0 + "\n"
-        let numberArray = result.1.components(separatedBy: CharacterSet(charactersIn: characterSet))
+        let numberArray = getNumberArrayFromString(originalString: numberString,numberString: result.1, delimiter: result.0)
         var sum = 0
         var negativeArray: [Int] = []
         for element in numberArray {
@@ -63,15 +63,41 @@ class StringCalculator {
     }
     
     private func getDelimiter(inputString: String) -> (String,String) {
-        if inputString.hasPrefix("//") {
+        if inputString.hasPrefix("//[") {
+            if let startIndex = inputString.firstIndex(of: "["), let endIndex = inputString.firstIndex(of: "]") {
+                let delimiter = String(inputString[startIndex...endIndex].dropFirst().dropLast())
+                if let index = inputString.firstIndex(of: "\n") {
+                    let startIndex = inputString.index(index, offsetBy: 1)
+                    let modifiedString = String(inputString[startIndex...])
+                    return (delimiter,modifiedString)
+                }
+            }
+        } else if inputString.hasPrefix("//") {
             if let index = inputString.firstIndex(of: "\n") {
                 let prefix = inputString.prefix(upTo: index)
                 let delimiter = String(prefix[prefix.index(prefix.startIndex, offsetBy: 2)])
                 let startIndex = inputString.index(index, offsetBy: 1)
                 let modifiedString = String(inputString[startIndex...])
-                return (delimiter,modifiedString)
+                return (delimiter+newLineDelimiter,modifiedString)
             }
         }
-        return (",",inputString)
+        return (","+newLineDelimiter,inputString)
+    }
+    
+    private func getNumberArrayFromString(originalString:String, numberString:String, delimiter: String) -> [String] {
+        var numberArray: [String] = []
+        if originalString.hasPrefix("//[") && originalString.numberOfOccurrencesOf(string: "[") == 1 && originalString.numberOfOccurrencesOf(string: "]") == 1 {
+            numberArray = numberString.components(separatedBy: delimiter)
+        } else {
+            numberArray = numberString.components(separatedBy: CharacterSet(charactersIn: delimiter))
+        }
+        return numberArray
+    }
+}
+
+
+extension String {
+    func numberOfOccurrencesOf(string: String) -> Int {
+        return self.components(separatedBy:string).count - 1
     }
 }
